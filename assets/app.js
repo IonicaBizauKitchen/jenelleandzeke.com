332eae55624f42232c9536b5da4d7b604edcda74
Zeke Sikelianos <zeke@sikelianos.com>
2015-02-19 21:27:23 -0800

    more refinements

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,25 @@
 $(function(){
 
   var api_url = "https://jenelleandzeke.firebaseio.com/attendees.json"
-  var existing = store.get('rsvp')
+
+  // Dump responses on the responses page
+  if ($("#responses").length) {
+    $.ajax({
+      type: "GET",
+      url: api_url,
+      success: function(responses) {
+        // $("#responses").text(JSON.stringify(data, null, 2))
+        console.table(responses)
+        // Object.keys(responses).forEach(function(key){
+        //   var response = responses[key]
+        //   $("#responses").append(JSON.stringify(response, null, 2))
+        // })
+      }
+    })
+  }
 
   // Pre-fill form if visitor already RSVP'd
+  var existing = store.get('rsvp')
   if (existing) {
     $("#rsvp-name").val(store.get('rsvp').name)
     $("#rsvp-attending-no").prop("checked", existing.attending === "no")
@@ -11,8 +27,10 @@ $(function(){
     $("#rsvp-talents").val(existing.talents)
   }
 
+  // Handle form submissions
   $("form.rsvp").submit(function(e){
 
+    // Munge form values into an objects
     var rsvp = {}
     $.each($('form.rsvp').serializeArray(), function(i, field) {
       rsvp[field.name] = field.value;
@@ -26,6 +44,7 @@ $(function(){
     // Save rsvp to localStorage
     store.set("rsvp", rsvp)
 
+    // Save rsvp to Firebase
     $.ajax({
       type: "POST",
       url: api_url,
@@ -39,9 +58,8 @@ $(function(){
       }
     })
 
-    // Prevent the form submission from redirecting the page
+    // Prevent form submission from redirecting the page
     return false
   })
-
 
 })
