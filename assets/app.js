dc5076b7eb35e7916285550a522e9b3a53267ef9
Zeke Sikelianos <zeke@sikelianos.com>
2015-02-19 21:03:14 -0800

    save form data and repopulate

--- assets/app.js
+++ assets/app.js
@@ -1,35 +1,46 @@
 $(function(){
 
-  var url = "https://jenelleandzeke.firebaseio.com/attendees.json"
+  var api_url = "https://jenelleandzeke.firebaseio.com/attendees.json"
+  var existing = store.get('rsvp')
+
+  // Pre-fill form if visitor already RSVP'd
+  if (existing) {
+    $("#rsvp-name").val(store.get('rsvp').name)
+    $("#rsvp-attending-no").prop("checked", existing.attending === "no")
+    $("#rsvp-guests").val(existing.guests)
+    $("#rsvp-talents").val(existing.talents)
+  }
 
   $("form.rsvp").submit(function(e){
 
-    var response = {};
+    var rsvp = {}
     $.each($('form.rsvp').serializeArray(), function(i, field) {
-      response[field.name] = field.value;
-      if (field.value) {
-        store.set(field.name, field.value);
-      }
+      rsvp[field.name] = field.value;
     });
 
-    if (!response.name) {
-      alert("Please provide your name")
+    if (!rsvp.name) {
+      alert("Please fill out your name")
       return false;
     }
 
-    console.log("response", response)
+    // Save rsvp to localStorage
+    store.set("rsvp", rsvp)
 
     $.ajax({
       type: "POST",
-      url: url,
-      data: JSON.stringify(response),
+      url: api_url,
+      data: JSON.stringify(rsvp),
       success: function(data) {
-        console.log("API response", data)
-        $(".form-thanks").show()
+        if (rsvp.attending === "yes") {
+          $(".form-result-happy").slideDown()
+        } else {
+          $(".form-result-sad").slideDown()
+        }
       }
-    });
+    })
 
-    return false;
+    // Prevent the form submission from redirecting the page
+    return false
   })
 
 
